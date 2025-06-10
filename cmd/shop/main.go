fcc04c8099202df564b90657cf0917f3ad50dbc6
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-06-09 21:53:13 -0400

    init commit: 24 hours later and I have my configuration system working xD

---
+++ cmd/shop/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+type LoggingConfig struct {
+	Encoding string `mapstructure:"encoding"`
+	Level    string `mapstructure:"level"`
+}
+
+func main() {
+	configFile := flag.String("config", "config.yaml", "Path to the configuration file.")
+	flag.Parse()
+	viper.SetConfigFile(*configFile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	loggingConfig := viper.GetStringMap("logging")
+	jsonData, err := json.Marshal(loggingConfig)
+	var cfg zap.Config
+	if err := json.Unmarshal(jsonData, &cfg); err != nil {
+		panic(err)
+	}
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Hello world!")
+}
