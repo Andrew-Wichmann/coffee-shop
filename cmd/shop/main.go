3898f04ad202c19a2a09c5d163480096d5bfef3c
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-06-10 22:02:36 -0400

    feat: add logging middleware

--- cmd/shop/main.go
+++ cmd/shop/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/Andrew-Wichmann/coffee-shop/pkg/logging"
+	"go.uber.org/zap"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		requestLogger := logging.Logger.With(
+			zap.String("method", req.Method), zap.String("requestURI", req.RequestURI),
+		)
+		requestLogger.Info("Request received")
+		start := time.Now()
+		next.ServeHTTP(w, req)
+		duration := time.Since(start)
+		requestLogger.Info("Request finished", zap.Duration("duration", duration))
+	})
+}
+
+func rootHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.WriteHeader(200)
+	rw.Write([]byte("Hello world"))
+}
+
 func main() {
-	logging.Logger.Debug("Hello world!")
-	logging.Logger.Info("Hello world!")
-	logging.Logger.Warn("Hello world!")
-	logging.Logger.Error("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	logging.Logger.Info("Starting server!")
+	http.ListenAndServe(":8080", loggingMiddleware(mux))
 }
