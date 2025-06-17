7ac351944346ccfaf9212d9023ec611f4c14e373
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-06-16 20:07:41 -0400

    reset the coffee-shop-cli

--- cmd/coffee-shop-cli/main.go
+++ cmd/coffee-shop-cli/main.go
@@ -1,35 +1,17 @@
 package main
 
 import (
-	"log"
-
-	_ "github.com/charmbracelet/bubbles"
 	tea "github.com/charmbracelet/bubbletea"
 	_ "github.com/charmbracelet/lipgloss"
-	"github.com/gorilla/websocket"
 )
 
 type app struct {
 	response string
-}
-
-var conn *websocket.Conn
-
-type requestError error
-type requestResp string
-
-func createConnection() tea.Msg {
-	log.Println("Connecting to websocket")
-	var err error
-	conn, _, err = websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
-	if err != nil {
-		return requestError(err)
-	}
-	return nil
+	views    []tea.Model
 }
 
 func (a app) Init() tea.Cmd {
-	return createConnection
+	return nil
 }
 
 func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -38,36 +20,14 @@ func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return a, tea.Quit
 		}
 		if msg.Type == tea.KeyEnter {
-			return a, sendRequest
+			return a, nil
 		}
-	}
-	if response, ok := msg.(requestResp); ok {
-		a.response = string(response)
-	}
-	if response, ok := msg.(requestError); ok {
-		a.response = string(response.Error())
 	}
 	return a, nil
 }
 
 func (a app) View() string {
-	if a.response != "" {
-		return a.response
-	}
 	return "Hello world"
-}
-
-func sendRequest() tea.Msg {
-	err := conn.WriteMessage(websocket.TextMessage, []byte("enter"))
-	if err != nil {
-		return requestError(err)
-	}
-	_, _msg, err := conn.ReadMessage()
-	if err != nil {
-		return requestError(err)
-	}
-	log.Println(string(_msg))
-	return requestResp(_msg)
 }
 
 func main() {
